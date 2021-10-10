44cd6911d3a8ad5f06b7aaa3968b9eb5146487e0
Ajith11@ <ajithselvan14@gmail.com>
2021-10-10 18:14:59 +0530

    updatethree

--- onlgro/urls.py
+++ onlgro/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-path('',views.TestFun,name='test'),
+
 path('contact',views.TestFuncont,name='contact'),
 path('contact1',views.newpg,name='contact1')
 ]
